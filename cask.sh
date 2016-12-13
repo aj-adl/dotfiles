5721d975476d768c3f5951ca5e8cb6910164f22a
Alex James Bishop <alex@framecreative.com.au>
2016-12-13 11:39:16 +1030

    add cask.sh file to install apps via homebrew cask

---
+++ cask.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# Only run after brew cask has been installed
+
+# get our favourite operating syst.... I mean browser
+
+brew cask install google-chrome
+
+# lets get our dev dependencies
+
+brew cask install phpstorm
+
+brew cask install sequel-pro
+
+brew cask install transmit
+
+brew cask install virtualbox
+
+brew cask install iterm2
+
+brew cask install gas-mask
+
+brew cask install flycut
+
+# yeah 2 text editors and an IDE, sue me
+brew cask install sublime-text
+brew cask install atom
+
+# some utilities and other shit
+
+brew cask install flux
+
+brew cask install skitch
+
+brew cask install slack
+
+brew cask install divvy
+
+brew cask install evernote
+
+brew cask install screenmailer
+
+# Evil apps that we cannot live without
+
+brew cask install dropbox
+
+brew cask install adobe-creative-cloud
